c71a6eff1e265026cb58cbefe37f2d8b2082e360
Skriptonit <122312257+Skriptonit@users.noreply.github.com>
2023-03-28 18:22:38 +0300

    Delete dz11 directory

--- dz11/2.py
+++
@@ -1,8 +0,0 @@
-def distance(a, n):
-    return pow(a, n)
-
-
-a = float(input())
-n = float(input())
-
-print(distance(a, n))
--- dz11/3.py
+++
@@ -1,5 +0,0 @@
-a = input()
-def capitalize(str):
-    str = str.title()
-    return str
-print(capitalize(a))
